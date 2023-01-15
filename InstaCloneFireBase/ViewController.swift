ddc9f907a8050502ecb3d1e264761c4c205adbed
Masoud Toorani <masoud.t92@gmail.com>
2023-01-15 22:03:25 +0300

    Initial Commit

---
+++ InstaCloneFireBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaCloneFireBase
+//
+//  Created by Masoud Toorani on 15.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
